9df34fa6cf75438e2282ad21c0ab9f43e9007bea
tehila-zlates <zab5858@gmail.com>
2026-01-19 14:46:03 +0200

    Update wines.ts
    jjj

--- project/src/service/wines.ts
+++ project/src/service/wines.ts
@@ -1,26 +1,26 @@
 import axios from 'axios';
 
 export function GetAllWines() {
-  return axios.get('api/http://localhost:2000/api/wines');
+  return axios.get('/api/http://localhost:2000/api/wines');
 }
 
 export function GetWinesById(id: number) {
-  return axios.get(`api/http://localhost:2000/api/wines/${id}`);
+  return axios.get(`/api/http://localhost:2000/api/wines/${id}`);
 }
 
 export function GetWinesByCategory(category: string) {
-  return axios.get(`api/http://localhost:2000/api/wines/category/${encodeURIComponent(category)}`);
+  return axios.get(`/api/http://localhost:2000/api/wines/category/${encodeURIComponent(category)}`);
 }
 
 export function AddWineReview(id: string, reviewObj: { clientId: string, clientName: string, comment: string }) {
-  return axios.post(`api/http://localhost:2000/api/wines/${id}/review`, reviewObj);
+  return axios.post(`/api/http://localhost:2000/api/wines/${id}/review`, reviewObj);
 }
 
 
 export function UploadWineImage(file: File) {
   const formData = new FormData();
   formData.append('image', file);
-  return axios.post('api/http://localhost:2000/api/wines/upload-image', formData, {
+  return axios.post('/api/http://localhost:2000/api/wines/upload-image', formData, {
     headers: { 'Content-Type': 'multipart/form-data' }
   });
 }
@@ -33,22 +33,22 @@ export function AddWine(wineData: {
   description: string;
   image: string;
 }) {
-  return axios.post('api/http://localhost:2000/api/wines', wineData);
+  return axios.post('/api/http://localhost:2000/api/wines', wineData);
 }
 
 export function DeleteWine(id: string) {
-  return axios.delete(`api/http://localhost:2000/api/wines/${id}`);
+  return axios.delete(`/api/http://localhost:2000/api/wines/${id}`);
 }
 
 export function CheckCustomer(email: string, password: string) {
-  return axios.post('api/http://localhost:2000/api/customers/login', {
+  return axios.post('/api/http://localhost:2000/api/customers/login', {
     email,
     password
   });
 }
 
 export function AddCustomer(name:string, email: string, password: string) {
-  return axios.post('api/http://localhost:2000/api/customers/signUp', {
+  return axios.post('/api/http://localhost:2000/api/customers/signUp', {
     name,
     email,
     password
@@ -56,7 +56,7 @@ export function AddCustomer(name:string, email: string, password: string) {
 }
 
 export function updateCustomer(id: string, name: string, email: string, password: string, isAdmin: boolean) {
-  return axios.put(`api/http://localhost:2000/api/customers/${id}`, {
+  return axios.put(`/api/http://localhost:2000/api/customers/${id}`, {
     id,
     name,
     email,
@@ -67,5 +67,5 @@ export function updateCustomer(id: string, name: string, email: string, password
 }
 
 export function deleteWineReview(wineId: string, reviewIndex: number) {
-  return axios.delete(`api/http://localhost:2000/api/wines/${wineId}/review/${reviewIndex}`);
+  return axios.delete(`/api/http://localhost:2000/api/wines/${wineId}/review/${reviewIndex}`);
 }
